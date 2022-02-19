f0dc0b13652b1bef05de06c761d58106992274f3
mjonesdev <mjones4114@outlook.com>
2022-02-19 19:42:52 +0000

    alter psql script

--- psql.sh
+++ psql.sh
@@ -4,20 +4,26 @@
 # Script for the installation and setup of PostgrSQL
 ##################################
 
+# Setup repo
+
+sudo dnf install -y https://download.postgresql.org/pub/repos/yum/reporpms/F-35-x86_64/pgdg-fedora-repo-latest.noarch.rpm;
+
 # Install PostgrSQL
 
-sudo dnf install -y postgresql postgresql-libs postgresql-plperl postgresql-plpython postgresql-server;
+sudo dnf install -y postgresql14-server;
 
 # Configure PostgreSQL
 
-## Initialise database
-postgresql-setup initdb
+## Initialise database and set to start on boot
+sudo /usr/pgsql-14/bin/postgresql-14-setup initdb
+sudo systemctl enable postgresql-14
+sudo systemctl start postgresql-14
 
-## Set to start on boot
-chkconfig postgresql on
 
 ## Start service
-service postgresql start
+postgresql-setup --initdb
+systemctl enable postgresql.service
+systemctl start postgresql.service
 
 ## create user and database
 
