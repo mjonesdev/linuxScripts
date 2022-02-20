ae2bdbb8dd3d362e6ae0f20ee68aba6ae641eabf
mjonesdev <mjones4114@outlook.com>
2022-02-20 14:17:56 +0000

    moved gnome tweaks changes to fish config file

--- fishAndThemeConfig.sh
+++ fishAndThemeConfig.sh
@@ -4,6 +4,7 @@
 # Do not run with sudo
 # Only run after software installs
 # Must be run after Fish has been manually started for the first time. Recommended to run using Fish to ensure config files in .config
+# Must open gnome-tweaks before running this script
 ############################################
 
 # Fish config
@@ -15,3 +16,11 @@ end
 
 neofetch
 EOF
+
+# Configure theme
+## You can use "dconf watch /" to find the commands required to programmatically add other options
+gsettings set org.gnome.shell enabled-extensions "['background-logo@fedorahosted.org', 'user-theme@gnome-shell-extensions.gcampax.github.com']";
+gsettings set org.gnome.desktop.interface gtk-theme 'Nordic-darker-v40';
+gsettings set org.gnome.shell.extensions.user-theme name 'Nordic-darker-v40';
+gsettings set org.gnome.desktop.interface icon-theme 'Zafiro-Icons-Dark-Blue-f';
+gsettings set org.gnome.desktop.background picture-uri 'file:///home/matt/.themes/941435.png';
--- softwareInstalls.sh
+++ softwareInstalls.sh
@@ -85,14 +85,4 @@ mv linuxScripts/theme_files/Nordic-darker-v40 ./.themes;
 mv linuxScripts/theme_files/Zafiro-Icons-Dark-Blue-f ./.icons;
 mv linuxScripts/theme_files/941435.png ./.themes;
 
-# Configure theme
-## You can use "dconf watch /" to find the commands required to programmatically add other options
-gnome-tweaks;
-gsettings set org.gnome.shell enabled-extensions "['background-logo@fedorahosted.org', 'user-theme@gnome-shell-extensions.gcampax.github.com']";
-gsettings set org.gnome.desktop.interface gtk-theme 'Nordic-darker-v40';
-gsettings set org.gnome.shell.extensions.user-theme name 'Nordic-darker-v40';
-gsettings set org.gnome.desktop.interface icon-theme 'Zafiro-Icons-Dark-Blue-f';
-gsettings set org.gnome.desktop.background picture-uri 'file:///home/matt/.themes/941435.png';
-
-
 # shutdown -r +0;
