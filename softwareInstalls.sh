4470ba4a001c45d73ce0390cbfe78bff62949b78
mjonesdev <mjones4114@outlook.com>
2022-02-20 13:06:27 +0000

    correct shell extension issue

--- softwareInstalls.sh
+++ softwareInstalls.sh
@@ -82,11 +82,11 @@ sudo dnf install gnome-tweaks -y;
 mkdir .themes;
 mkdir .icons;
 mv linuxScripts/theme_files/Nordic-darker-v40 ./.themes;
-mv linusScripts/theme_files/Zafiro-Icons-Dark-Blue-f ./.icons;
+mv linuxScripts/theme_files/Zafiro-Icons-Dark-Blue-f ./.icons;
 
 # Configure theme
 ## You can use "dconf watch /" to find the commands required to programmatically add other options
-gsettings set .org.gnome.shell enabled-extensions ['background-logo@fedorahosted.org', 'user-theme@gnome-shell-extensions.gcampax.github.com'];
+gsettings set org.gnome.shell enabled-extensions "['background-logo@fedorahosted.org', 'user-theme@gnome-shell-extensions.gcampax.github.com']";
 gsettings set org.gnome.desktop.interface gtk-theme 'Nordic-darker-v40';
 gsettings set org.gnome.shell.extensions.user-theme name 'Nordic-darker-v40';
 gsettings set org.gnome.desktop.interface icon-theme 'Zafiro-Icons-Dark-Blue-f';
